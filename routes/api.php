300ed3194a332ae05efcfa171de1b9be8541ff71
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-05 22:28:08 +0300

    feat: (endpoints) api endpoints added to api.php for teams and fixtures domains

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\FixtureController;
+use App\Http\Controllers\FootballMatchController;
+use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/teams', function (Request $request) {
-    //
+Route::prefix('teams')->group(function () {
+    Route::get('/', [TeamController::class, 'getAllTeams']);
+    Route::get('/{team_id}', [TeamController::class, 'getTeam']);
 });
 
-Route::get('/fixtures', function (Request $request) {
-    //
-});
-
-Route::get('/matches', function (Request $request) {
-    //
+Route::prefix('fixtures')->group(function () {
+    Route::get('/', [FixtureController::class, 'getAllWeeklyFixtures']);
+    Route::get('/{fixture_id}', [FixtureController::class, 'getWeeklyFixture']);
+    Route::post('/', [FixtureController::class, 'generateLeagueFixtures']);
+    Route::post('/simulate-week', [FixtureController::class, 'simulateWeek']);
+    Route::post('/simulate-all', [FixtureController::class, 'simulateAllWeeks']);
+    Route::delete('/', [FixtureController::class, 'resetAllFixturesAndFootballMatches']);
 });
