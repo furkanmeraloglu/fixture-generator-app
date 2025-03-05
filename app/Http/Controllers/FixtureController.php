18c405ea78498d37b201dcdff9a0ca479ca55c1d
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-05 22:21:37 +0300

    feat: (fixture-controller) Fixture controller created and get, post and delete endpoints formed

--- app/Http/Controllers/FixtureController.php
+++ app/Http/Controllers/FixtureController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Fixture\GenerateLeagueFixturesService;
+use App\Services\Fixture\ReadAllWeeklyFixturesService;
+use App\Services\Fixture\ReadWeeklyFixtureService;
+use App\Services\Fixture\ResetLeagueFixturesService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FixtureController extends Controller
 {
-    //
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getAllWeeklyFixtures(Request $request): JsonResponse
+    {
+        try {
+            $data = (new ReadAllWeeklyFixturesService($request))->boot();
+            return $this->getSuccessfulResponse($data, 'All weekly fixtures returned successfully');
+        } catch (\Throwable $th) {
+            return $this->getErrorResponse($th->getMessage(), $th->getCode());
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param int $fixture_id
+     * @return JsonResponse
+     */
+    public function getWeeklyFixture(Request $request, int $fixture_id): JsonResponse
+    {
+        try {
+            $data = (new ReadWeeklyFixtureService($request, $fixture_id))->boot();
+            return $this->getSuccessfulResponse($data, 'Weekly fixture returned successfully');
+        } catch (\Throwable $th) {
+            return $this->getErrorResponse($th->getMessage(), $th->getCode());
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function generateLeagueFixtures(Request $request): JsonResponse
+    {
+        try {
+            $data = (new GenerateLeagueFixturesService($request))->boot();
+            return $this->getSuccessfulResponse($data, 'League fixtures generated successfully', Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            return $this->getErrorResponse($th->getMessage(), $th->getCode());
+        }
+    }
+
+    public function resetAllFixturesAndFootballMatches(Request $request): JsonResponse
+    {
+        try {
+            $data = (new ResetLeagueFixturesService($request))->boot();
+            return $this->getSuccessfulResponse($data, 'League fixtures has been reset successfully');
+        } catch (\Throwable $th) {
+            return $this->getErrorResponse($th->getMessage(), $th->getCode());
+        }
+    }
 }
