5ef79a480713a372de11737f2492bafa60867707
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-05 20:23:13 +0300

    feat: (team-model) Attributes and relations added to Team model

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
@@ -41,4 +43,43 @@ class Team extends Model
         'goals_conceded' => 'integer',
         'deleted_at' => 'datetime',
     ];
+
+    protected $appends = [
+        'team_all_matches',
+        'team_total_score_average'
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function homeMatches(): HasMany
+    {
+        return $this->hasMany(FootballMatch::class, 'home_team_id', 'team_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function awayMatches(): HasMany
+    {
+        return $this->hasMany(FootballMatch::class, 'away_team_id', 'team_id');
+    }
+
+    /**
+     * @return array
+     */
+    public function getTeamAllMatchesAttribute(): array
+    {
+        return $this->homeMatches
+            ->merge($this->awayMatches)
+            ->toArray();
+    }
+
+    /**
+     * @return int
+     */
+    public function getTeamTotalScoreAverageAttribute(): int
+    {
+        return $this->goals_scored - $this->goals_conceded;
+    }
 }
