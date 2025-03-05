bd5d80e965d6b7b66c397bc2d0c633ed3f675f8c
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-05 20:22:43 +0300

    refactor: (controller-abstract) Controller abstract phpdocs added for better static analyses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 abstract class Controller
 {
+    /**
+     * @param array|object $data
+     * @param string $message
+     * @param int $code
+     * @return JsonResponse
+     */
     public function getSuccessfulResponse(array|object $data, string $message = "Successful", int $code = Response::HTTP_OK): JsonResponse
     {
         return response()->json([
@@ -18,6 +24,12 @@ abstract class Controller
         ]);
     }
 
+    /**
+     * @param string $message
+     * @param int $code
+     * @param array|object|null $errors
+     * @return JsonResponse
+     */
     public function getErrorResponse(string $message = "Something went wrong.", int $code = Response::HTTP_BAD_REQUEST, array|object $errors = null): JsonResponse
     {
         return response()->json([
