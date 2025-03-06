7d5e133672fee7f3f248c77149d0f953be97b166
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-06 23:20:24 +0300

    feat: (team-factory) Team factory constructed for testing purposes

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -17,7 +17,15 @@ class TeamFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name . ' FC',
+            'strength' => $this->faker->numberBetween(70, 100),
+            'points' => 0,
+            'goals_scored' => 0,
+            'goals_conceded' => 0,
+            'wins' => 0,
+            'losses' => 0,
+            'draws' => 0,
+            'played_matches' => 0,
         ];
     }
 }
