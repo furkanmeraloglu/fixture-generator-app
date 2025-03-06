12e30bb68069ecd4370cc97f13a3eb760b32ca0f
furkanmeraloglu <furkan.meraloglu@iceberg-digital.co.uk>
2025-03-06 21:12:29 +0300

    test: (team-read) team read service tests created

---
+++ tests/Unit/TeamReadAllServiceTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Team;
+use App\Services\Team\TeamReadAllService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class TeamReadAllServiceTest  extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_boot_function_returns_all_teams_without_ordering()
+    {
+        $first_team = Team::factory()->create(['name' => 'Besiktas JK', 'strength' => 90]);
+        $second_team = Team::factory()->create(['name' => 'Galatasaray SK', 'strength' => 80]);
+        $third_team = Team::factory()->create(['name' => 'Fenerbahce SK', 'strength' => 85]);
+        $fourth_team = Team::factory()->create(['name' => 'Trabzonspor', 'strength' => 75]);
+
+        $request = new Request();
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertCount(4, $response);
+        $this->assertContains($first_team->team_id, array_column($response, 'team_id'));
+        $this->assertContains($second_team->team_id, array_column($response, 'team_id'));
+        $this->assertContains($third_team->team_id, array_column($response, 'team_id'));
+        $this->assertContains($fourth_team->team_id, array_column($response, 'team_id'));
+    }
+
+    public function test_boot_function_returns_all_teams_ordered_by_strength()
+    {
+        $first_team = Team::factory()->create(['name' => 'Besiktas JK', 'strength' => 90]);
+        $second_team = Team::factory()->create(['name' => 'Galatasaray SK', 'strength' => 80]);
+        $third_team = Team::factory()->create(['name' => 'Fenerbahce SK', 'strength' => 85]);
+        $fourth_team = Team::factory()->create(['name' => 'Trabzonspor', 'strength' => 75]);
+
+        $request = new Request(['__order_by' => '-strength']);
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertCount(4, $response);
+        $this->assertEquals(90, $response[0]['strength']);
+        $this->assertEquals(85, $response[1]['strength']);
+        $this->assertEquals(80, $response[2]['strength']);
+        $this->assertEquals(75, $response[3]['strength']);
+
+        $request = new Request(['__order_by' => 'strength']);
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertCount(4, $response);
+        $this->assertEquals(75, $response[0]['strength']);
+        $this->assertEquals(80, $response[1]['strength']);
+        $this->assertEquals(85, $response[2]['strength']);
+        $this->assertEquals(90, $response[3]['strength']);
+    }
+
+    public function test_boot_function_returns_all_teams_ordered_by_name()
+    {
+        $first_team = Team::factory()->create(['name' => 'Besiktas JK', 'strength' => 90]);
+        $second_team = Team::factory()->create(['name' => 'Galatasaray SK', 'strength' => 80]);
+        $third_team = Team::factory()->create(['name' => 'Fenerbahce SK', 'strength' => 85]);
+        $fourth_team = Team::factory()->create(['name' => 'Trabzonspor', 'strength' => 75]);
+
+        $request = new Request(['__order_by' => '-name']);
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertCount(4, $response);
+        $this->assertEquals('Trabzonspor', $response[0]['name']);
+        $this->assertEquals('Galatasaray SK', $response[1]['name']);
+        $this->assertEquals('Fenerbahce SK', $response[2]['name']);
+        $this->assertEquals('Besiktas JK', $response[3]['name']);
+
+        $request = new Request(['__order_by' => 'name']);
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertCount(4, $response);
+        $this->assertEquals('Besiktas JK', $response[0]['name']);
+        $this->assertEquals('Fenerbahce SK', $response[1]['name']);
+        $this->assertEquals('Galatasaray SK', $response[2]['name']);
+        $this->assertEquals('Trabzonspor', $response[3]['name']);
+    }
+
+    public function test_boot_function_returns_null_array_if_there_is_no_teams()
+    {
+        $request = new Request();
+
+        $response = (new TeamReadAllService($request))->boot();
+
+        $this->assertIsArray($response);
+        $this->assertEmpty($response);
+    }
+}
---
+++ tests/Unit/TeamReadServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Exceptions\DataNotFoundException;
+use App\Models\Team;
+use App\Services\Team\TeamReadService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TeamReadServiceTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_boot_function_returns_team_data_when_team_exists()
+    {
+        $team = Team::factory()->create([
+            'name' => 'Besiktas JK',
+            'strength' => 90
+        ]);
+        $response = (new TeamReadService($team->team_id))->boot();
+
+        $this->assertIsArray($response);
+        $this->assertEquals($team->team_id, $response['team_id']);
+        $this->assertEquals('Besiktas JK', $response['name']);
+        $this->assertEquals(90, $response['strength']);
+        $this->assertArrayHasKey('team_all_matches', $response);
+    }
+
+    public function test_boot_function_throws_exception_when_team_does_not_exist()
+    {
+        $nonExistentTeamId = 9999;
+
+        $this->expectException(DataNotFoundException::class);
+        $this->expectExceptionMessage('Team could not be found!');
+
+        (new TeamReadService($nonExistentTeamId))->boot();
+    }
+}
